40e71b7d3bebf42d93b2c449e01bae3ea040965a
tavoxr <tavoxr23@gmail.com>
2021-08-16 05:24:48 -0600

    set upt  base template

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,26 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="wrapper">
+  <Navbar/>    
+  <section class="section">
+    <router-view/>
+  </section>
+  <footer class="footer">
+    <p class="has-text-centered">Copyright (c) 2021</p>
+  </footer>
   </div>
-  <router-view/>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
+<script>
+import Navbar from './components/Navbar.vue'
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+export default {
+  name:'App',
+  components:{
+    Navbar
   }
 }
+</script>
+<style lang="scss">
+@import '../node_modules/bulma';
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,84 @@
+<template>
+    <nav class="navbar is-dark">
+      <div class="navbar-brand">
+          <router-link to="/" class="navbar-item">
+            <strong>Ecomm</strong>
+          </router-link>
+
+          <a role="button"  class="navbar-burger" 
+                            aria-label="menu" 
+                            aria-expanded="false" 
+                            data-target="navbarMenu"
+                            
+                            >
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
+      </div> 
+
+      <div id="navbarMenu" class="navbar-menu"   >
+        <div class="navbar-end">
+          <router-link to="/summer" class="navbar-item">Summer</router-link>
+          <router-link to="/winter" class="navbar-item">Winter</router-link>
+          
+          <div class="navbar-item">
+              <div class="buttons">
+                <router-link to="/log-in"  class="button is-light">Log in</router-link>
+
+                <router-link to="/cart"  class="button is-success">
+                <span class="icon"><i class="fas fa-shopping-cart"></i></span>
+                <span class="">Cart</span>
+                </router-link>
+
+              </div>
+          </div>
+        </div>
+
+      </div>
+    </nav>
+</template>
+
+
+<script>
+export default {
+    name: 'Navbar',
+    data(){
+        return{
+            showMobileMenu: false
+        }
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+  // Check if there are any navbar burgers
+  if ($navbarBurgers.length > 0) {
+
+    // Add a click event on each of them
+    $navbarBurgers.forEach( el => {
+      el.addEventListener('click', () => {
+
+        // Get the target from the "data-target" attribute
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
+
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+
+      });
+    });
+  }
+
+});
+
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
