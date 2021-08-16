221cbba34f76217e703677d9c0a6eea0ac1440a8
tavoxr <tavoxr23@gmail.com>
2021-08-16 12:43:49 -0600

    latestProducts page

---
+++ src/components/Product.vue
@@ -0,0 +1,28 @@
+<template>
+     <div class="box">
+        <figure class="image mb-4">
+          <img v-bind:src="product.get_thumbnail" />
+        </figure>
+        <h3 class="is-size-4">{{product.name}}</h3>
+        <p class="is-size-6 has-text-grey">$ {{product.price}}</p>
+        
+        View details
+
+      </div> 
+</template>
+
+<script>
+
+export default {
+    name: 'Product',
+    props: {
+        product : Object
+    }
+    
+    
+}
+</script>
+
+
+
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,62 @@
 <template>
   <div class="home">
-    Home
+    <section class="hero is-medium is-dark mb-6">
+    <div class="hero-body has-text-centered"> 
+      <p class="title mb-6">
+        Welcome to MyEcomm
+        </p>
+         <p class="subtitle">
+        The best store online
+        </p>
+    </div>
+    </section>
+
+    <div class="columns is-multiline">
+      <div class="column is-12"> 
+          <h2 class="is-size-2 has-text-centered">Latest products</h2>
+      </div>
+      <div class="column is-3" 
+           v-for="product in latestProducts"
+           v-bind:key="product.id"
+           >
+     
+          <Product  :product="product" />
+
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-
+import Product from '../components/Product.vue'
+import axios from 'axios'
 
 export default {
   name: 'Home',
   components: {
-  
+    Product
+  },
+  data(){
+    return {
+      latestProducts : []
+      }
+  },
+  mounted(){
+    this.getLatestProducts()
+  },
+  methods:{
+    async getLatestProducts(){
+
+      try{
+       const response = await axios.get('/api/latest-products/')
+       console.log('latest-products', response.data)
+       this.latestProducts = response.data
+      }catch(error){
+          console.log(error)
+      }
+    }
   }
+
 }
 </script>
