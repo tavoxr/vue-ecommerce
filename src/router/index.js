dd8794706f9114881ec525f969b2f16bbdddd267
tavoxr <tavoxr23@gmail.com>
2021-08-21 09:16:55 -0600

    signup view

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Product from '../views/Product.vue';
 import Category from '../views/Category.vue';
 import Search from '../views/Search.vue';
 import Cart from '../views/Cart.vue';
+import SignUp from '../views/SignUp.vue';
 
 const routes = [
   {
@@ -39,6 +40,11 @@ const routes = [
     path: '/cart',
     name: 'Cart',
     component: Cart
+  },
+  {
+    path: '/sign-up',
+    name: 'SignUp',
+    component: SignUp
   }
 ]
 
