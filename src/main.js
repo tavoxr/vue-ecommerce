62113ff2539c9ea162968492a7e6b535e62c8861
tavoxr <tavoxr23@gmail.com>
2021-08-23 19:23:08 -0600

    axios baseUrl

--- src/main.js
+++ src/main.js
@@ -4,6 +4,6 @@ import router from './router'
 import store from './store'
 import axios from 'axios'
 
-axios.defaults.baseURL = 'https://vue-ecomm-tavox.netlify.app'
+axios.defaults.baseURL = 'https://drf-ecomm-api.herokuapp.com'
 
 createApp(App).use(store).use(router, axios).mount('#app')
