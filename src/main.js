4b2ab1640e9d1339375ec1c250d2450db49a58f4
tavoxr <tavoxr23@gmail.com>
2021-08-31 17:52:05 -0600

    dbs

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import store from './store'
 import axios from 'axios'
 
-axios.defaults.baseURL = 'https://drf-ecomm-api.herokuapp.com'
+//axios.defaults.baseURL = 'https://drf-ecomm-api.herokuapp.com'
 
+axios.defaults.baseURL = 'http://localhost:8000'
 createApp(App).use(store).use(router, axios).mount('#app')
