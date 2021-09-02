fc1df0c2fb50558d2df6575e23e1b925d52b178c
tavoxr <tavoxr23@gmail.com>
2021-09-02 02:46:06 -0600

    baseUrl changed

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 import store from './store'
 import axios from 'axios'
 
-//axios.defaults.baseURL = 'https://drf-ecomm-api.herokuapp.com'
+axios.defaults.baseURL = 'https://vue-dj-ecomm.herokuapp.com'
 
-axios.defaults.baseURL = 'http:///127.0.0.1:8000'
+//axios.defaults.baseURL = 'http:///127.0.0.1:8000'
 createApp(App).use(store).use(router, axios).mount('#app')
